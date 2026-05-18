7a8715d66f62c8698c5fceb2f151519cd747c913
Dmitry Petrov <Dima.demon.net@mail.ru>
2026-05-18 19:50:52 +0400

    feat: добавил стили для мейн лейаута

--- packages/client/src/app/layouts/MainLayout.tsx
+++ packages/client/src/app/layouts/MainLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
-
 import { Header } from '@/widgets/header'
+import styles from './MainLayout.module.scss'
 
 export const MainLayout = () => (
   <>
     <Header />
-    <main>
+    <main className={styles.main}>
       <Outlet />
     </main>
   </>
