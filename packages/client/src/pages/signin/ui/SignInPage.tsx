5684e037a8a105042ac3ef10dab43153978e02ca
Maxim Steinbrecher <maximste@gmail.com>
2026-05-21 00:02:10 +0300

    Реализована страница входа

--- packages/client/src/pages/signin/ui/SignInPage.tsx
+++ packages/client/src/pages/signin/ui/SignInPage.tsx
@@ -1,21 +1,29 @@
 import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
 
 import { usePage } from '@/app/hooks/usePage'
+import { SignInForm } from '@/features/sign-in'
 
 import { initSignInPage } from '../model/initSignInPage'
+
+import styles from './SignInPage.module.scss'
 
 export const SignInPage = () => {
   usePage({ initPage: initSignInPage })
 
   return (
-    <div>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Вход</title>
-        <meta name="description" content="Авторизация" />
+        <title>Вход — Catsweeper</title>
+        <meta name="description" content="Войдите, чтобы общаться на форуме" />
       </Helmet>
-      <h1>Вход</h1>
-      <p>Авторизация (заглушка)</p>
-    </div>
+      <section className={styles.page}>
+        <SignInForm />
+        <Link className={styles.back} to="/">
+          ← На главную
+        </Link>
+      </section>
+    </>
   )
 }
