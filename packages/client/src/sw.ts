6dae29509bcf641b796af1c689958e125ea8025a
Maxim Steinbrecher <maximste@gmail.com>
2026-06-03 09:45:23 +0300

    Добавлены стадии ЖЦ service work: install и activate

--- packages/client/src/sw.ts
+++ packages/client/src/sw.ts
@@ -26,4 +26,26 @@ const STATIC_URLS: readonly string[] = [
   '/img/rating3-icon.png',
 ]
 
-export { CACHE_NAME, STATIC_URLS }
+const sw = self as unknown as ServiceWorkerGlobalScope
+
+sw.addEventListener('install', (event: ExtendableEvent) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll([...STATIC_URLS]))
+  )
+  sw.skipWaiting()
+})
+
+sw.addEventListener('activate', (event: ExtendableEvent) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        )
+      )
+      .then(() => sw.clients.claim())
+  )
+})
+
+export {}
