6bd5ce550eedf77d162f92342bfa68c6c4b41302
Dmitry Petrov <Dima.demon.net@mail.ru>
2026-07-04 14:43:41 +0400

    feat: feat(server): добавлены модели Sequelize Topic/Comment/Reply/Reaction

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,12 +5,20 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 import express from 'express'
 import { connectDb } from './db'
+import { syncModels } from './models'
 
 const app = express()
 app.use(cors())
 const port = Number(process.env.SERVER_PORT) || 3001
 
-connectDb()
+const bootstrapDb = async () => {
+  const connected = await connectDb()
+  if (connected) {
+    await syncModels()
+  }
+}
+
+bootstrapDb()
 
 app.get('/', (_, res) => {
   res.json({ message: 'Hello from API' })
