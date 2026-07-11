9dc3ab94acf1b92e1bf3734f5ccf34ef6aeace8c
Dmitrii.Andriianov <dmitrii.andriianov@itpb.spb.ru>
2026-07-11 15:41:45 +0300

    Исправлены ошибки линта

--- packages/server/types/express-request.d.ts
+++ packages/server/types/express-request.d.ts
@@ -1,15 +1,8 @@
+import type { AuthUser } from '../auth'
+
 declare module 'express-serve-static-core' {
   interface Request {
-    user?: {
-      id: number
-      first_name: string
-      second_name: string
-      display_name?: string | null
-      login: string
-      email: string
-      phone: string
-      avatar?: string | null
-    }
+    user?: AuthUser
   }
 }
 
